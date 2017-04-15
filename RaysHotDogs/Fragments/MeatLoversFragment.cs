9b1b11243f5cc4a9621c96761bc59d2ae7147c4e
Other-pc\Admin <ninetailsdevelopment@gmail.com>
2017-04-15 19:36:12 +0100

    Adding more fragments

--- RaysHotDogs/Fragments/MeatLoversFragment.cs
+++ RaysHotDogs/Fragments/MeatLoversFragment.cs
@@ -14,7 +14,7 @@ using RaysHotDogs.Adapters;
 
 namespace RaysHotDogs.Fragments
 {
-    public class FavouriteHotDogFragment : BaseFragment
+    public class MeatLoversFragment : BaseFragment
     {
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -26,19 +26,18 @@ namespace RaysHotDogs.Fragments
         public override void OnActivityCreated(Bundle savedInstanceState)
         {
             base.OnActivityCreated(savedInstanceState);
+
             FindViews();
 
             HandleEvents();
 
-            hotDogs = hotDogDataService.GetFavouriteHotDogs();
+            hotDogs = hotDogDataService.GetHotDogsForGroup(1);
             listView.Adapter = new HotDogListAdapter(this.Activity, hotDogs);
         }
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            // Use this to return your custom view for this Fragment
-            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
-
-            return inflater.Inflate(Resource.Layout.FavouriteHotDogFragment, container, false);
+            return inflater.Inflate(Resource.Layout.MeatLoversFragment, container, false);
         }
     }
 }
